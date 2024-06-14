317bc031683dd96eb44df979c5018eecf1260d86
Utkarsh Tripathi <utripathi2002@gmail.com>
2024-06-14 23:15:09 +0530

    add: boilerplate
    Signed-off-by: Utkarsh Tripathi <utripathi2002@gmail.com>

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Setup
+
+Run `npm i` after cloning the repo
+
+## Contributing
+
+Make changes and create PR, the commits should be signed like `git commit -s -m` ; use the `-s` flag. Rememeber to add detailed docs in your PRs.
+
+#### Don't forget to star the repo
---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const { createClient } = require('redis')
+const server = http.createServer(app)
+
+async function notifyFriends(msg){
+    
+}
+
+const main = async () => {
+    const redisClient = createClient()
+
+    redisClient.on('error', err => console.log('Redis Client Error', err));
+
+    await redisClient.connect()
+
+    const { Server } = require('socket.io')
+    const io = new Server(server)
+
+    io.on('connection',(socket)=>{
+        console.log("A user connected", socket.id)
+        socket.on('disconnect',()=>{
+            console.log("A user disconnected")
+        })
+
+        socket.on('online',async (msg)=>{
+            await redisClient.set(msg,'online')
+            console.log('user is online', msg)
+            notifyFriends(msg)
+        })
+        
+        socket.on('offline',async (msg)=>{
+            await redisClient.set(msg,'offline')
+            console.log('user is offline', msg)
+            notifyFriends(msg)
+        })
+    })
+
+    server.listen(3000, ()=>{
+        console.log("Listening on 3000")
+    })
+}
+
+main()
